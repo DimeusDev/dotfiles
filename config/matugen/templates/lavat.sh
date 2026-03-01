3ebffb9ddc03da7ffd165d5a4ea8a2c8c81fe7d1
DimeusDev <aantonin1234@gmail.com>
2026-03-01 18:40:50 +0100

    adjusted lavat speed and hyprexpo columns

--- config/matugen/templates/lavat.sh
+++ config/matugen/templates/lavat.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-lavat -g -c "{{colors.source_color.default.hex_stripped}}" -k "{{colors.on_primary.default.hex_stripped}}" -R 2 -s 2
+lavat -g -c "{{colors.source_color.default.hex_stripped}}" -k "{{colors.on_primary.default.hex_stripped}}" -R 2 -s 1
 exec "$0"
